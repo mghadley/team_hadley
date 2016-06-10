d176d7700d4225c96efcb590f0ef27007b5db9f3
mghadley <matthew.g.hadley@gmail.com>
2016-06-09 21:37:32 -0600

    setting up models and controllers

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the home template" do
+    	get :home
+    	expect(response).to render_template(:home)
+    end
+  end
+
+end
