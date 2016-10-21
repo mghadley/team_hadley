f8e81c0e07ba1660a213ad9d12af6dbdf375222c
mghadley <matthew.g.hadley@gmail.com>
2016-10-20 22:43:43 -0600

    updating deploy file to precompile locally

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,6 +46,23 @@ namespace :puma do
 end
 
 namespace :deploy do
+
+  namespace :assets do
+    task :precompile, :roles => :web do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+        run_locally("rake assets:clean && rake assets:precompile")
+        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+        run_locally "rm public/assets.tar.bz2"
+        run_locally("rake assets:clean")
+      else
+        logger.info "Skipping asset precompilation because there were no asset changes"
+      end
+    end
+  end
+    
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
